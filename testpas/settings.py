9d941c010d215de8f8f6e0f4662d2f9bd5753c51
sonvule59 <svu23@iastate.edu>
2026-01-28 12:47:11 -0600

    fix avatar size for wr challenge 14

--- testpas/settings.py
+++ testpas/settings.py
@@ -284,7 +284,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
