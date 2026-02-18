b50223c48203eea0761e9997b14243c78907bc65
Ellie Chen <echen@iastate.edu>
2026-02-18 17:51:05 -0600

    rough first draft of walking activity and statistics

--- Frontend/CM_Intervention/src/App.jsx
+++ Frontend/CM_Intervention/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WalkingActivityDemo from './WalkingActivityDemo.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       {/* Add whatever here as you need */}
+      <WalkingActivityDemo></WalkingActivityDemo>
     </>
   )
 }
