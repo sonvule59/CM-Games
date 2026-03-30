0d0f7077f83f0cabebb7891d81c2cc5c9d9fdf45
Ellie Chen <echen@iastate.edu>
2026-03-30 16:48:18 -0500

    add indoor domestic activity routing

--- Frontend/CM_Intervention/src/App.jsx
+++ Frontend/CM_Intervention/src/App.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import OfficeGame from './Componets/OfficeGame'
 import RockClimbing from './Componets/rockClimbing'
 import WalkingActivity from './Componets/WalkingActivity'
+import IndoorDomesticActivity from './Componets/IndoorDomesticActivity'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Route path="/office" element = {<OfficeGame/>} />
       <Route path="/walk" element = {<WalkingActivity />} />
       <Route path="/rock" element = {<RockClimbing /> } />
+      <Route path="/indoor-domestic" element = {<IndoorDomesticActivity /> } />
     </Routes>
     </BrowserRouter>
   
---
+++ Frontend/CM_Intervention/src/Componets/IndoorDomesticActivity.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+type IndoorDomesticActivityProps = {};
+
+function IndoorDomesticActivity({}: IndoorDomesticActivityProps) {}
+
+export default IndoorDomesticActivity;
