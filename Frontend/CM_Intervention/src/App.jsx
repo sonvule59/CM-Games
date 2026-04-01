3259c46d68f5145e8ac082049fa5fa014698e749
pbkelly <pbkelly@iastate.edu>
2026-04-01 18:52:20 -0500

    Some quick updates hot fix 1

--- Frontend/CM_Intervention/src/App.jsx
+++ Frontend/CM_Intervention/src/App.jsx
@@ -9,6 +9,8 @@ import OfficeGame from './Componets/OfficeGame'
 import WalkingActivityDemo from './Componets/WalkingActivityDemo.tsx'
 import RockClimbing from './Componets/rockClimbing'
 import OfficeGameStart from './Componets/OfficeGameStart.jsx'
+import LeisureHome from './Componets/leisureHome.jsx'
+import OutdoorsActivities from "./Componets/outdoorsActivities"
 
 
 function App() {
@@ -17,8 +19,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Default route when you hit "/" */}
+        {/* Home page routes */}
         <Route path="/" element={<LeisureHome Start/>} />
+        <Route path='/office' element={<OfficeGameStart />} />
         
 
         {/* Individual routes for each mini‑game */}
@@ -26,6 +29,7 @@ function App() {
         <Route path="/rock" element={<RockClimbing />} />
         <Route path="/walk" element={<WalkingActivityDemo />} />
         <Route path='/office/test' element = {<OfficeGame/>} /> 
+
     </Routes>
     </BrowserRouter>
   );
