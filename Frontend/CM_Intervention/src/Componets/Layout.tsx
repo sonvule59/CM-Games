7c32a95bc60eca5bdebb7c8464ea2e56da0fb371
Ellie Chen <echen@iastate.edu>
2026-04-14 12:45:41 -0500

    add more layout components

--- Frontend/CM_Intervention/src/Componets/Layout.tsx
+++ Frontend/CM_Intervention/src/Componets/Layout.tsx
@@ -2,6 +2,12 @@
 
 import { rcStyles } from "../Static/rockClimbingStyles";
 
+type InheritProps<
+    InheritedElement extends React.ElementType,
+    NewProps extends Record<string, any> = {},
+> = Omit<React.ComponentPropsWithoutRef<InheritedElement>, keyof NewProps> &
+    NewProps;
+
 export function addClassNameToProps<T extends { className?: string } & object>(
     props: T,
     className: string,
@@ -9,50 +15,70 @@ export function addClassNameToProps<T extends { className?: string } & object>(
     return { ...props, className: `${className} ${props.className ?? ""}` };
 }
 
-export function Container(props: React.ComponentPropsWithoutRef<"div">) {
+export function Container(props: InheritProps<"div">) {
     return <div {...addClassNameToProps(props, rcStyles.container)} />;
 }
 
-export function TopRow(props: React.ComponentPropsWithoutRef<"div">) {
+export function TopRow(props: InheritProps<"div">) {
     return <div {...addClassNameToProps(props, rcStyles.topRow)} />;
 }
 
-export function Section(props: React.ComponentPropsWithoutRef<"div">) {
+export function Section(props: InheritProps<"div">) {
     return <div {...addClassNameToProps(props, rcStyles.section)} />;
 }
 
-export function Title(props: React.ComponentPropsWithoutRef<"div">) {
+export function Title(props: InheritProps<"div">) {
     return <div {...addClassNameToProps(props, rcStyles.title)} />;
 }
 
-export function Subtitle(props: React.ComponentPropsWithoutRef<"div">) {
+export function MainTitle(props: InheritProps<"div">) {
+    return <div {...addClassNameToProps(props, rcStyles.mainTitle)} />;
+}
+
+export function ScenePill(props: InheritProps<"div">) {
+    return <div {...addClassNameToProps(props, rcStyles.scenePill)} />;
+}
+
+export function Subtitle(props: InheritProps<"div">) {
     return <div {...addClassNameToProps(props, rcStyles.subtitle)} />;
 }
 
-export function Paragraph(props: React.ComponentPropsWithoutRef<"p">) {
+export function Paragraph(props: InheritProps<"p">) {
     return <p {...addClassNameToProps(props, rcStyles.paragraph)} />;
 }
 
-export function Header(props: React.ComponentPropsWithoutRef<"div">) {
+export function Header(props: InheritProps<"div">) {
     return <div {...addClassNameToProps(props, rcStyles.header)} />;
 }
 
-export function HeaderLeft(props: React.ComponentPropsWithoutRef<"div">) {
+export function HeaderLeft(props: InheritProps<"div">) {
     return <div {...addClassNameToProps(props, rcStyles.headerLeft)} />;
 }
 
-export function HeaderSubtitle(props: React.ComponentPropsWithoutRef<"div">) {
+export function HeaderRight(props: InheritProps<"div">) {
+    return (
+        <div
+            {...addClassNameToProps(
+                props,
+                "flex flex-wrap items-center gap-2 justify-end",
+            )}
+        />
+    );
+}
+
+export function HeaderSubtitle(props: InheritProps<"div">) {
     return <div {...addClassNameToProps(props, rcStyles.headerSubtitle)} />;
 }
 
-export function Button(
-    props: { isPrimary?: boolean } & React.ComponentPropsWithoutRef<"button">,
-) {
+export function Button({
+    isPrimary,
+    ...props
+}: InheritProps<"button", { isPrimary?: boolean }>) {
     return (
         <button
             {...addClassNameToProps(
                 props,
-                (props.isPrimary ?? true)
+                (isPrimary ?? true)
                     ? rcStyles.primaryButton
                     : rcStyles.secondaryButton,
             )}
@@ -60,12 +86,26 @@ export function Button(
     );
 }
 
-export function PrimaryButton(props: React.ComponentPropsWithoutRef<"button">) {
+export function PrimaryButton(props: InheritProps<"button">) {
     return <Button {...props} isPrimary={true} />;
 }
 
-export function SecondaryButton(
-    props: React.ComponentPropsWithoutRef<"button">,
-) {
+export function SecondaryButton(props: InheritProps<"button">) {
     return <Button {...props} isPrimary={false} />;
+}
+
+export function ResetButton(props: InheritProps<"button">) {
+    return (
+        <button {...addClassNameToProps(props, rcStyles.resetButton)}>
+            {"children" in props ? props.children : <>Reset</>}
+        </button>
+    );
+}
+
+export function BackButton(props: InheritProps<"button">) {
+    return (
+        <SecondaryButton>
+            {"children" in props ? props.children : <>Back</>}
+        </SecondaryButton>
+    );
 }
