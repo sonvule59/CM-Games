68cc17a90a095eb4c10e183e1b1a0356576dd139
Ellie Chen <echen@iastate.edu>
2026-04-20 18:15:56 -0500

    set up type safety for React Router framework
    see: https://reactrouter.com/how-to/route-module-type-safety

--- Frontend/CM_Intervention/src/routes.ts
+++ Frontend/CM_Intervention/src/routes.ts
@@ -1,6 +1,24 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    // * matches all URLs, the ? makes it optional so it will match / as well
-    route("*?", "App.jsx"),
+    // Top-level hub
+    index("./Componets/homeOfHomePages.jsx"),
+    route("/leisure", "./Componets/leisureHome.jsx"),
+    route("/office", "./Componets/OfficeGameStart.jsx"),
+    route("/mindfulness-home", "./Componets/mindfulnessHome.jsx"),
+    route("/mindfulness", "./Componets/mindfulnessGame.jsx"),
+    route("/transport-home", "./Componets/TransportGameStart.jsx"),
+
+    // Individual routes for each mini-game
+    route("/outdoors", "./Componets/outdoorsActivities.jsx"),
+    route("/rock", "./Componets/rockClimbing.jsx"),
+    route("/walk", "./Componets/WalkingActivity.tsx"),
+    route("/office/test", "./Componets/OfficeGame.jsx"),
+    route("/swim", "./Componets/SwimmingActivity.tsx"),
+    route("/indoor-domestic", "./Componets/IndoorDomesticActivity.tsx"),
+    route("/domestic-home", "./Componets/domesticHome.jsx"),
+    route("/outside-domestic", "./Componets/outsideDomestic.jsx"),
+    route("/transport", "./Componets/TransportGame.jsx"),
+    route("/parking", "./Componets/ParkingLot.jsx"),
+    route("/transport2", "./Componets/TransportGame2.jsx"),
 ] satisfies RouteConfig;
