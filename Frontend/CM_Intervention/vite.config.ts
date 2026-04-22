188966528faffcaccc1898aee0077362a44ed5cf
pbkelly <pbkelly@iastate.edu>
2026-04-21 22:04:08 -0500

    I am dumb my bad yall

--- Frontend/CM_Intervention/vite.config.ts
+++ Frontend/CM_Intervention/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [reactRouter(), tailwindcss()],
 server: {
     host: true, 
     port: 5173
